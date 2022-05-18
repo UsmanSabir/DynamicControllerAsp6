99cb8c499ea9701bf54d6d3251b4ed652038672e
Usman Sabir <hisabir@hotmail.com>
2022-05-18 12:58:49 +0500

    dev activator

---
+++ DynamicControllerPOC/CustomServices/MyControllerActivator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+
+namespace DynamicControllerPOC.CustomServices;
+
+public class MyControllerActivator : IControllerActivator
+{
+    private readonly ILogger _logger;
+
+    public MyControllerActivator(ILogger<MyControllerActivator> logger)
+    {
+        _logger = logger;
+    }
+
+    public object Create(ControllerContext context)
+    {
+        var serviceProvider = context.HttpContext.RequestServices;
+        Type type = context.ActionDescriptor.ControllerTypeInfo.AsType();
+        //var inst =  Activator.CreateInstance(type);
+        var inst2= serviceProvider.GetRequiredService(type); //
+        return inst2;
+    }
+
+    public void Release(ControllerContext context, object controller)
+    {
+        if (controller is IDisposable disposable)
+        {
+            disposable.Dispose();
+        }
+    }
+}
--- DynamicControllerPOC/Program.cs
+++ DynamicControllerPOC/Program.cs
@@ -1,8 +1,20 @@
+using DynamicControllerPOC.CustomServices;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var feature = new ControllerFeature();
+builder.Services.AddControllers().PartManager.PopulateFeature(feature);
+foreach (var featureController in feature.Controllers)
+{
+    builder.Services.TryAddTransient(featureController.AsType());
+}
 
-builder.Services.AddControllers();
+builder.Services.AddSingleton<IControllerActivator, MyControllerActivator>();
+builder.Services.Replace(ServiceDescriptor.Transient<IControllerActivator, MyControllerActivator>());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
